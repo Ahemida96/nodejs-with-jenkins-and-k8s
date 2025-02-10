032ec895a1bd692e08ba3021f51e66a711b6de72
Meda <ahmedhemida121@gmail.com>
2025-02-10 12:37:28 +0200

    Update files

--- command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo docker run -p 8080:8080 -p 50000:50000 -d \
--v jenkins_home:/var/jenkins_home \
--v /var/run/docker.sock:/var/run/docker.sock \
--v "$(which docker)":/usr/bin/docker jenkins/jenkins:lts
-
-"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"
