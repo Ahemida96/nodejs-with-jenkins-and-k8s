d766cc57f1b3c290b8479d37c40dc543dd28e466
Meda <ahmedhemida121@gmail.com>
2025-02-10 02:40:12 +0200

    update gitignore

--- command.sh
+++ command.sh
@@ -3,3 +3,5 @@ sudo docker run -p 8080:8080 -p 50000:50000 -d \
 -v jenkins_home:/var/jenkins_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v "$(which docker)":/usr/bin/docker jenkins/jenkins:lts
+
+"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"
